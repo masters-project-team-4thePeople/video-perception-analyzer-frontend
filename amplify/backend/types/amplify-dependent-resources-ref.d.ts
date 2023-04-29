8112d9956433865e734fdece72414facb46e2e52
Shreya-07 <shreya.nimbhorkar@gmail.com>
2023-04-28 22:44:19 -0700

    Amplify new setup

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "vpa23d1b070": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "vpas3": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
